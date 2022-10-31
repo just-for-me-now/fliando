091d298fc996e2495e8d9ff49faddee31b6cee96
Javier Padin <javier.padin@solera.com>
2022-10-31 14:29:54 +0100

    created projects

---
+++ flights/flights/src/test/java/com/fliando/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fliando.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ price/price/src/test/java/com/fliando/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fliando.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
