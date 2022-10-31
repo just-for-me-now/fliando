4fa98287d9b328902418c851a8f38814e2f9511c
Javier Padin <javier.padin@solera.com>
2022-10-31 16:08:23 +0100

    change log -> logs

--- logs/log/src/main/java/com/fliando/log/controller/Controller.java
+++ logs/log/src/main/java/com/fliando/log/controller/Controller.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +18,13 @@ public class Controller {
 	@Autowired
 	private LogService service;
 
-	@PostMapping(path="/log")
+	@PostMapping(path="/logs")
 	public ResponseEntity<Object> saveLog(@RequestBody String logText) {
 		service.saveLog(logText);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
-	@GetMapping(path ="/log")
+	@GetMapping(path ="/logs")
 	public List<String> retreiveAllData() {
 		return service.findAllLogs();
 	}
