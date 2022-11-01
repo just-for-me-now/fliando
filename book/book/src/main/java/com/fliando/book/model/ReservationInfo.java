2cbf484e4af64df02076535d3c4d6c878e3f09d2
David Paradela <david.paradela@solera.com>
2022-11-01 11:57:41 +0100

    fix important stuff

---
+++ book/book/src/main/java/com/fliando/book/model/ReservationInfo.java
@@ -0,0 +1,93 @@
+package com.fliando.book.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class ReservationInfo {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(name="flight-id")
+	private long flightId;
+	
+	private int toddlers;
+	
+	private int children;
+	
+	private int adults;
+	
+	private int luggage;
+	
+	public ReservationInfo() {
+	}
+	
+	
+
+	public ReservationInfo(long flightId, int toddlers, int children, int adults, int luggage) {
+		super();
+		this.flightId = flightId;
+		this.toddlers = toddlers;
+		this.children = children;
+		this.adults = adults;
+		this.luggage = luggage;
+	}
+
+
+
+	public long getFlightId() {
+		return flightId;
+	}
+
+	public void setFlightId(long flightId) {
+		this.flightId = flightId;
+	}
+
+	public int getToddlers() {
+		return toddlers;
+	}
+
+	public void setToddlers(int toddlers) {
+		this.toddlers = toddlers;
+	}
+
+	public int getChildren() {
+		return children;
+	}
+
+	public void setChildren(int children) {
+		this.children = children;
+	}
+
+	public int getAdults() {
+		return adults;
+	}
+
+	public void setAdults(int adults) {
+		this.adults = adults;
+	}
+
+	public int getLuggage() {
+		return luggage;
+	}
+
+	public void setLuggage(int luggage) {
+		this.luggage = luggage;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservationInfo [id=" + id + ", flightId=" + flightId + ", toddlers=" + toddlers + ", children="
+				+ children + ", adults=" + adults + ", luggage=" + luggage + "]";
+	}
+
+	
+	
+}
---
+++ book/book/src/test/java/com/fliando/book/APITests.java
@@ -0,0 +1,35 @@
+package com.fliando.book;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.fliando.book.model.ReservationInfo;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
+
+public class APITests {
+
+	private static RequestSpecification requestSpec;
+	
+	@BeforeAll
+	public static void setup() {
+		requestSpec = new RequestSpecBuilder()
+				.setBaseUri("http://localhost:8085")
+				.build();
+	}
+	
+	@Test
+	public void Push_WrongDestination_CannotAccessDestination() {
+		//flight id; toddlers; children; adults; luggage;
+		ReservationInfo info = new ReservationInfo(404,0,0,1,1);
+		given(requestSpec)
+		.when()
+			.body(info)
+			.post("/book")
+		.then()
+			.assertThat().statusCode(400);
+	}
+}
