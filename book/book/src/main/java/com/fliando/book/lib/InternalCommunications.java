81bab23801ec1a9a0eadcf2d82d54b70e6bb6ac7
David Paradela <david.paradela@solera.com>
2022-11-01 12:05:07 +0100

    small fixes and internalCommunications on book

--- book/book/src/main/java/com/fliando/book/lib/InternalCommunications.java
+++ book/book/src/main/java/com/fliando/book/lib/InternalCommunications.java
@@ -1,10 +1,11 @@
-package com.fliando.flights.lib;
+package com.fliando.book.lib;
 
 import static io.restassured.RestAssured.given;
 
 import io.restassured.http.ContentType;
 
 public class InternalCommunications {
+
 	public static void post(String address, String body) {
 		given().contentType(ContentType.JSON).body(body).
 		post(address);
