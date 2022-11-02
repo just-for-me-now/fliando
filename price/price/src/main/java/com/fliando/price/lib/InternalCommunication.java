70395a5b5f76116d70afd9a30550e851f980de35
Javier Padin <javier.padin@solera.com>
2022-11-02 09:04:15 +0100

    changes to internal communications

--- price/price/src/main/java/com/fliando/price/lib/InternalCommunication.java
+++ price/price/src/main/java/com/fliando/price/lib/InternalCommunication.java
@@ -7,6 +7,18 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.*;
 
 public class InternalCommunication {
+	
+	private static String address = "http://localhost:8085/logs";
+	
+	public static void log(String body) {
+		try {
+			given().contentType(ContentType.JSON).body(body).
+			post(address);
+		} catch(Exception e) {
+			// TODO: Maybe log the fact that the other microservice is not available?
+			// We mainly are interested on decoupling from failure to respond
+		}
+	}
 
 	public static Flight get(String address, long id) {
 		return given().contentType(ContentType.JSON).get(address + "/" + id).as(Flight.class);
