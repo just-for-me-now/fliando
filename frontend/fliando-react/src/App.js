a4c9acc6462403e00b902d55b29c305c6681d58a
David Paradela <david.paradela@solera.com>
2022-11-02 15:59:21 +0100

    minor fixes

--- frontend/fliando-react/src/App.js
+++ frontend/fliando-react/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 
 import './App.css';
 import Header from './components/Header/Header.js';
 import SelectFlight from './components/SelectFlight/SelectFlight.js';
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <Header />
