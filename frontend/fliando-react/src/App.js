c6d2756785ecb7b2c8d024ddd4c124e311783dd6
Javier Padin <javier.padin@solera.com>
2022-11-03 14:26:02 +0100

    changes on flight frontend

--- frontend/fliando-react/src/App.js
+++ frontend/fliando-react/src/App.js
@@ -1,25 +1,19 @@
-import './App.css';
-import Header from './components/Header/Header.js';
-import FormSearch from './components/Form/FormSearch';
-import FlightContainer from './components/Flights/FlightContainer';
-import PassengerForm from './components/PassengerForm/PassengerForm';
+import "./App.css";
+import Header from "./components/Header/Header.js";
+import FormSearch from "./components/Form/FormSearch";
+import FlightContainer from "./components/Flights/FlightContainer";
+import PassengerForm from "./components/PassengerForm/PassengerForm";
 
 function App() {
-
-
-
-  return (
-    <>
-     <div className="App">
-    <Header/>
-    <h2 className="titleh2">Where are you flying to?</h2>
-    <FormSearch/>
-    <FlightContainer />
-    <PassengerForm />
-    </div>
-    </>
-   
-  );
+	return (
+		<div className="App">
+			<Header />
+			<h2 className="titleh2">Where are you flying to?</h2>
+			<FormSearch />
+			<FlightContainer />
+			<PassengerForm />
+		</div>
+	);
 }
 
 export default App;
--- frontend/fliando-react/src/components/Flights/Flight.js
+++ frontend/fliando-react/src/components/Flights/Flight.js
@@ -1,9 +1,43 @@
 import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
+import './Flight.css';
+import { useState } from "react";
 
-const Flight = () => {
+const Flight = props => {
+
+    const [selected, setSelected] = useState(false);
+
+    const handleSelect = () => {
+        props.select(props.id);
+        setSelected(true);
+    }
+    
+    const handleCancel = () => {
+        props.deselect();
+        setSelected(false);
+    }
+
     return (
         <Card className="flightBox flight">
-            Each flight goes here
+            <div className="flight-top"><div className="flight-airline">{props.flight.airline}</div></div>
+
+            <div className="flight-main">
+                <div className="flight-itinerary">
+                    <div className="flight-origin">{props.flight.origin}</div>
+                    <div className="flight-arrow">{"->"}</div>
+                    <div className="flight-destination">{props.flight.destination}</div>
+                </div>
+                <div className="flight-date">
+                    <div className="flight-day">{props.flight.time.date}</div>
+                    <div className="flight-day">{props.flight.time.time}</div>
+                </div>
+            </div>
+
+            <div className="flight-bottom">
+            {props.flight.scales ? <div className="flight-scales"> this plane has scales</div> : ""}
+            <div className="price-per-adult">€ {props.flight.pricePerAdult}</div>
+            {selected ? <Button onClick={handleCancel}>Cancel</Button> : <Button onClick={handleSelect}>Select</Button>}
+            </div>
         </Card>
     )
 }
