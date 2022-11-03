d1a24a5da0d19d0d6662b81614756ad4230b6fcb
Javier Padin <javier.padin@solera.com>
2022-11-03 09:54:37 +0100

    small changes on flight frontend

---
+++ frontend/fliando-react/src/components/Flights/FlightContainer.js
@@ -0,0 +1,12 @@
+import Flight from "./Flight";
+
+const FlightContainer = () => {
+    return (
+        <div>
+            <Flight />
+            <Flight />
+        </div>
+    );
+}
+
+export default FlightContainer;
