1fab9e49bbd978d49fdbd48d496a8ba46b77df88
David Paradela <david.paradela@solera.com>
2022-11-04 12:16:12 +0100

    small fixes

--- frontend/fliando-react/src/components/Admin/AdminStats.js
+++ frontend/fliando-react/src/components/Admin/AdminStats.js
@@ -4,13 +4,13 @@ import useFetch from "../UseFetch/UseFetch";
 
 const AdminStats = () =>{
 
-    const address = `http://localhost:8085/accounts`;
+    const address = `http://localhost:8085/counts`;
     console.log(address)
     const { data } = useFetch(address);
 
     return (
         <div>
-            {data?.map(elem=><Counter name={data.name} counter={data.counter}></Counter>)}
+            {data?.map(elem=><Counter name={elem.name} counter={elem.counter}></Counter>)}
         </div>
         
     )
