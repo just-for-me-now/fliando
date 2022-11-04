6caa85d3d441317ca912286ca15bf078add54db7
David Paradela <david.paradela@solera.com>
2022-11-04 16:12:11 +0100

    admin react page

--- users/src/main/java/com/fliando/users/controller/Controller.java
+++ users/src/main/java/com/fliando/users/controller/Controller.java
@@ -2,6 +2,7 @@ package com.fliando.users.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import com.fliando.users.model.UserInfo;
 import com.fliando.users.service.UserService;
 
 @RestController
+@CrossOrigin
 public class Controller {
 	
 	private UserService service;
